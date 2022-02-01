86ed55ae93a8faa9e7e5a43206e5c39ebe006f25
Flávio Ferreira <business.flavioferreira@gmail.com>
2022-02-01 07:47:36 -0300

    connecting to the database

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"myapp/internal/driver"
 	"net/http"
 	"os"
 	"time"
@@ -50,7 +51,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4001, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application enviorment {development|production|maintenance}")
-
+	flag.StringVar(&cfg.db.dsn, "dsn", "admin:admin@tcp(localhost:3306)/widgets?parseTime=true&tls=false", "DSN")
 	flag.Parse()
 
 	cfg.stripe.key = os.Getenv("STRIPE_KEY")
@@ -59,6 +60,12 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	conn, err := driver.OpenDB(cfg.db.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer conn.Close()
+
 	app := &application{
 		config:   cfg,
 		infoLog:  infoLog,
@@ -66,7 +73,7 @@ func main() {
 		version:  version,
 	}
 
-	err := app.serve()
+	err = app.serve()
 	if err != nil {
 		log.Fatal(err)
 	}
